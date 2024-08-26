03342fff9dec13e7fbf1537afef0b2fa3270472f
karthik-wonderbiz <karthik.balakrishnan@wonderbiz.in>
2024-08-26 12:12:11 +0530

    add

--- client/src/app/modules/user/user.module.ts
+++ client/src/app/modules/user/user.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    UserDashboardComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule,
-    RouterModule,
-    FormsModule,
-    BaseChartDirective,
-    FullCalendarModule,
-    HttpClientModule
+    UserRoutingModule
   ]
 })
 export class UserModule { }
